0549a9e94c12463efabdc8716a56567a807305f9
Shafalil Gupta <sgupta@stuy.edu>
2018-04-25 04:48:37 -0400

    added tests

--- QQDriver.java
+++ QQDriver.java
@@ -9,20 +9,20 @@
  ******************************************************/
 
 public class QQDriver {
-    
+
     public static void main( String[] args ) {
 
 	System.out.println( "-INITIATING TESTING-" );
-	
+
 	System.out.println( "Creating New Double-Ended Queue..." );
 	QQKachoo<String> squashy = new QQKachoo();
-	
+
 	System.out.println( "Testing addFirst()..." );
 	squashy.addFirst( "be" );
 	squashy.addFirst( "it" );
 	System.out.println( "squashy:" );
-	System.out.println( squashy.toString() );	
-	
+	System.out.println( squashy.toString() );
+
 	System.out.println( "Testing addLast()..." );
 	squashy.addLast( "like" );
 	squashy.addLast( "that" );
@@ -34,15 +34,15 @@ public class QQDriver {
 
 	System.out.println( "Testing peekLast()..." );
 	System.out.println( squashy.peekLast() ); // that
-	
+
 	System.out.println( "Testing pollFirst()..." );
 	System.out.println( squashy.pollFirst() ); // it
 	System.out.println( squashy.pollFirst() ); // be
 	System.out.println( squashy.pollFirst() ); // like
-	
+
 	System.out.println( "Testing isEmpty()..." );
 	System.out.println( squashy.isEmpty() ); // false
-	
+
 	System.out.println( "Testing pollLast()..." );
 	System.out.println( squashy.pollLast() ); // that
 
@@ -50,11 +50,74 @@ public class QQDriver {
 	System.out.println( squashy.isEmpty() ); // true
 
 	System.out.println( "-TERMINATING TESTING-" );
-	
+
+  QQKachoo<String> iscream = new QQKachoo<String>();
+
+    System.out.println("-----Adding to front------");
+    iscream.addFirst("Chocolate");
+    System.out.println(iscream.peekFirst()); //Chocolate
+    iscream.addFirst("Vanilla");
+    System.out.println(iscream.peekFirst()); //Vanilla
+    iscream.addFirst("Strawberry");
+    System.out.println(iscream.peekFirst()); //Strawberry
+    iscream.addFirst("Mint");
+    System.out.println(iscream.peekFirst()); //Mint
+
+    System.out.println("");
+
+    System.out.println("------Current Deque------");
+    System.out.println(iscream); //Mint Strawberry Vanilla Chocolate
+
+    System.out.println("");
+
+    System.out.println("-----Adding to end------");
+    iscream.addLast("Pistachio");
+    System.out.println(iscream.peekLast()); //Pistachio
+    iscream.addLast("Coffee");
+    System.out.println(iscream.peekLast()); //Coffee
+    iscream.addLast("S'mores");
+    System.out.println(iscream.peekLast()); //S'mores
+
+    System.out.println("");
+
+    System.out.println("------Current Deque------");
+    System.out.println(iscream); //Mint Strawberry Vanilla Chocolate Pistachio Coffee S'mores
+
+    System.out.println("");
+
+    System.out.println("-----Removing from front-----");
+    iscream.pollFirst(); //Mint  dies
+    System.out.println(iscream.peekFirst()); //Strawberry
+
+    System.out.println("");
+
+    System.out.println("-----Removing from end-----");
+    iscream.pollLast(); //S'mores dies
+    System.out.println(iscream.peekLast()); //Coffee
+
+    System.out.println("");
+
+    System.out.println("------Current Deque------");
+    System.out.println(iscream); //Mint Strawberry Vanilla Chocolate Pistachio Coffee S'mores
+
+    System.out.println("");
+
+    System.out.println("------REMOVING EVERYTHING------");
+    iscream.pollFirst();
+    System.out.println(iscream);
+    iscream.pollLast();
+    System.out.println(iscream);
+    iscream.pollFirst();
+    System.out.println(iscream);
+    iscream.pollLast();
+    System.out.println(iscream);
+    iscream.pollFirst();
+    System.out.println(iscream);
+    System.out.println("Empty now?: "+iscream.isEmpty()); //true
 	/* ----- SQUASH ---------
 	   -------- SQUAD -------- */
-	
+
     } // end of main method
 
-    
+
 } // end of QQDriver class
