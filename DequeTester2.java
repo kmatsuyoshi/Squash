f91230065ec9d0d26f971a5b0b73859c378ffafa
Kenny <lli15@stuy.edu>
2018-04-25 08:39:18 -0400

    dequeTester

---
+++ DequeTester2.java
@@ -0,0 +1,42 @@
+// Team Ayo (Shafali, Henry, Kenny)
+// APCS2 pd1
+// L #02: All Hands on Deque!
+// 2018-04-25w
+
+/*****************************************************
+ * class QQDriver
+ * Driver for QQKachoo
+ ******************************************************/
+
+public class DequeTester2 {
+
+    public static void main(String[] args) {
+	QQKachoo<String> ayo = new QQKachoo<String>();
+
+	//Testing add methods
+	System.out.println("Add first: cat");
+	ayo.addFirst("cat");
+	System.out.println("Add first: dog");
+	ayo.addFirst("dog");
+	System.out.println("Add first: icecream");
+	ayo.addFirst("icecream");
+	System.out.println("Add last: pizza");
+	ayo.addLast("pizza");
+	System.out.println("Add last: tower");
+	ayo.addLast("tower");
+
+	//Testing toString
+	System.out.println("Print toString: (icecream, dog, cat, pizza, tower)");
+	System.out.println(ayo);
+
+	//Testing peek
+	System.out.println("PeekFirst: icecream");
+	System.out.println(peekFirst());
+	System.out.println("PeekLast: tower");
+	System.out.println(peekLast());
+
+	//Testing poll
+	System.out.println(pollLast());
+	System.out.println(pollFirst());
+    }
+}
